42a4006dc4a6f37205bd873c4aa9c937acd9921d
Estudiante USC <estudiante@GTACAD.USC>
2022-10-14 16:31:52 -0500

    Entrega

---
+++ ActividadOnce/src/Persona.java
@@ -0,0 +1,138 @@
+
+public class Persona {
+
+	private final static char sexoDeffault = 'H';
+	
+	public static final int bajoPeso = -1;
+	
+	public static final int pesoIdeal = 0;
+	
+	public static final int sobrePeso = 1;
+	
+	private String n;
+	
+	private int edad;
+	
+	private String id;
+	
+	private char sexo;
+	
+	private double w;
+	
+	private double h;
+
+	public Persona() {
+		this("", 0, sexoDeffault, 0, 0);
+	}
+
+	public Persona(String n, int edad, char sexo) {
+		this(n, edad, sexo, 0, 0);
+	}
+
+	public Persona(String n, int edad, char sexo, double w, double h) {
+		this.n = n;
+		this.edad = edad;
+		generarId();
+		this.sexo = sexo;
+		this.w = w;
+		this.h = h;
+	}
+	
+	@SuppressWarnings("unused")
+	private void comprobarSexo() {
+		if((sexo != 'H'||sexo != 'h') && (sexo != 'M'||sexo != 'm')) {
+			this.sexo=sexoDeffault;
+		}
+	}
+	
+	private void generarId() {
+		final int divisor = 23;
+		
+		int numId = ((int)Math.floor(Math.random()*(100000000 - 10000000)+10000000));
+		
+		int r = numId - (numId / divisor * divisor);
+		
+		char letraId = generaLetraId(r);
+		
+		id = Integer.toString(numId)+letraId;
+	}
+	
+	private char generaLetraId(int r) {
+		char letras[]= {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
+		return letras[r];
+	}
+/*
+	public String getN() {
+		return n;
+	}*/
+
+	public void setN(String n) {
+		this.n = n;
+	}
+
+	/*public int getEdad() {
+		return edad;
+	}*/
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	/*public char getSexo() {
+		return sexo;
+	}*/
+
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
+
+	/*public double getW() {
+		return w;
+	}*/
+
+	public void setW(double w) {
+		this.w = w;
+	}
+
+	/*public double getH() {
+		return h;
+	}*/
+
+	public void setH(double h) {
+		this.h = h;
+	}
+	
+	public int calcularIMC() {
+		double pesoR = w / (Math.pow(h, 2));
+		
+		if (pesoR >= 20 && pesoR <=25) {
+			return pesoIdeal;
+		} else if (pesoR < 20) {
+			return bajoPeso;
+		} else {
+			return sobrePeso;
+		}
+	}
+	
+	public boolean esMayorDeEdad() {
+		boolean mayor = false;
+		if(edad>=18) {
+			mayor = true;
+		}
+		return mayor;
+	}
+
+	@Override
+	public String toString() {
+		String sexo;
+		if(this.sexo == 'H') {
+			sexo = "hombre";
+		}
+		else {
+			sexo = "mujer";
+		}
+		return "Informacion personal: \n" + "Nombre: " + n + "\n" + "Sexo: " + sexo + "\n" + "Edad: " + edad + " años\n" + "Identificacion: " + id + "\n" + "Peso: " + w + " Kg\n" + "Altura: " + h + " metros\n";
+				
+	}
+	
+}
